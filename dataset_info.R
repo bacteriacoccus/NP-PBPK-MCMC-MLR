f5ed409de1e35ac31a19df552ba5b888cba97679
Parul <parulsharma1419@gmail.com>
2026-06-19 17:38:12 +0530

    Update CSV file paths in dataset_info.R

--- dataset_info.R
+++ dataset_info.R
@@ -8,7 +8,7 @@ ls_np_name = c("Au: Study1_12nm_0.85mg/kg","Au: Study1_23nm_0.85mg/kg","Au: Stud
 
 read_observation_data <- function(np_name) {
   if (np_name == "Au: Study1_12nm_0.85mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/gold/R_input_mouse_Study1_12nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/gold/R_input_mouse_Study1_12nm.csv")
     Obs.df <- Obs.df[1:7,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -16,7 +16,7 @@ read_observation_data <- function(np_name) {
     folder <- 'plots/Au/S1_12nm_nsc_GI/'
   } 
   else if (np_name == "Au: Study1_23nm_0.85mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/gold/R_input_mouse_Study1_23nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/gold/R_input_mouse_Study1_23nm.csv")
     Obs.df <- Obs.df[1:7,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -24,7 +24,7 @@ read_observation_data <- function(np_name) {
     folder <- 'plots/Au/S1_23nm_nsc_GI/'
   } 
   else if (np_name == "Au: Study1_100nm_0.85mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/gold/R_input_mouse_study1_100nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/gold/R_input_mouse_study1_100nm.csv")
     Obs.df <- Obs.df[1:7,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -33,7 +33,7 @@ read_observation_data <- function(np_name) {
   } 
   else if (np_name == "Au: Study2_34.6nm_3mg/kg"){
     #--------------------------Study 2: 34.6nm------------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/gold/Study2_PEG_coated_AuNPs_34.6nm.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/gold/Study2_PEG_coated_AuNPs_34.6nm.csv") 
     Obs.df <- Obs.df[1:6,]
     pathway <- Obs.df$Input.pathway[1]
     PDOSE = Obs.df$Dose.mg.kg.[1]
@@ -42,7 +42,7 @@ read_observation_data <- function(np_name) {
   }
   else if (np_name == "Au: Study2_55.5nm_3mg/kg"){
     #--------------------------Study 3: 55.5nm------------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/gold/Study2_PEG_coated_AuNPs_55.5nm.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/gold/Study2_PEG_coated_AuNPs_55.5nm.csv") 
     Obs.df <- Obs.df[1:8,]
     pathway <- Obs.df$Input.pathway[1]
     PDOSE = Obs.df$Dose..mg.kg.[1]
@@ -50,7 +50,7 @@ read_observation_data <- function(np_name) {
     folder = 'plots/Au/S2_55.5_nsc_GI/'}
   else if (np_name == "Au: Study2_77.1nm_3mg/kg"){
     #--------------------------Study 3: 77.1nm------------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/gold/Study2_PEG_coated_AuNPs_77.1nm.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/gold/Study2_PEG_coated_AuNPs_77.1nm.csv") 
     Obs.df <- Obs.df[1:8,]
     PDOSE = Obs.df$Dose..mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -58,7 +58,7 @@ read_observation_data <- function(np_name) {
     folder = 'plots/Au/S2_77.1_nsc_GI/'}
   else if (np_name == "Au: Study2_82.6nm_3mg/kg"){
     #--------------------------Study 3: 82.6nm------------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/gold/Study2_PEG_coated_AuNPs_82.6nm.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/gold/Study2_PEG_coated_AuNPs_82.6nm.csv") 
     Obs.df <- Obs.df[1:8,]
     PDOSE = Obs.df$Dose..mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -67,7 +67,7 @@ read_observation_data <- function(np_name) {
     }
   else if (np_name == "Au: Study3_27.6nm_4.26mg/kg"){
     #------------------Study3: 27.6 nm high dose-------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/gold/Study3_Gold dextran_h.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/gold/Study3_Gold dextran_h.csv") 
     Obs.df <- Obs.df[1:5,]
     pathway <- Obs.df$Input.pathway[1]
     PDOSE = Obs.df$Dose.mg.kg.[1]
@@ -78,7 +78,7 @@ read_observation_data <- function(np_name) {
   }
   else if (np_name == "Au: Study3_27.6nm_0.85mg/kg"){
     #------------------Study3: 27.6 nm low dose-------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/gold/Study3_Gold dextran_l.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/gold/Study3_Gold dextran_l.csv") 
     Obs.df <- Obs.df[1:5,]
     pathway <- Obs.df$Input.pathway[1]
     PDOSE = Obs.df$Dose.mg.kg.[1]
@@ -88,7 +88,7 @@ read_observation_data <- function(np_name) {
     
   }
   else if (np_name == "Si: Study1_20nm_10mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/silica/Study1_125I-SiNPs 20nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/silica/Study1_125I-SiNPs 20nm.csv")
     Obs.df <- Obs.df[1:5,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -96,7 +96,7 @@ read_observation_data <- function(np_name) {
     folder <- 'plots/silica/S1_20nm/'
   } 
   else if (np_name == "Si: Study1_80nm_10mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/silica/Study1_125I-SiNPs 80nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/silica/Study1_125I-SiNPs 80nm.csv")
     Obs.df <- Obs.df[1:5,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -104,7 +104,7 @@ read_observation_data <- function(np_name) {
     folder <- 'plots/silica/S1_80nm/'
   } 
   else if (np_name == "GO: Study1_20nm_20mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/GO/Study1_125I-NGS-PEG 10-30nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/GO/Study1_125I-NGS-PEG 10-30nm.csv")
     Obs.df <- Obs.df[1:5,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -112,7 +112,7 @@ read_observation_data <- function(np_name) {
     folder <- 'plots/GO/S1_20nm/'
   } 
   else if (np_name == "GO: Study2_243nm_1mg/kg") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/GO/Study2_125I-s-GO 243nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/GO/Study2_125I-s-GO 243nm.csv")
     Obs.df <- Obs.df[1:5,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -125,7 +125,7 @@ read_observation_data <- function(np_name) {
     Obs.df$Time[3] = 10/60
   } 
   else if (np_name == "GO: Study2_914nm_1mg/kg_all") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/GO/Study2_125I-l-GO 914nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/GO/Study2_125I-l-GO 914nm.csv")
     Obs.df <- Obs.df[1:5,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -138,7 +138,7 @@ read_observation_data <- function(np_name) {
     Obs.df$Time[3] = 10/60
   } 
   else if (np_name == "GO: Study2_914nm_1mg/kg_w/o_CS") {
-    Obs.df <- read.csv(file = "dataset_1/tk/mouse/GO/Study2_125I-l-GO 914nm.csv")
+    Obs.df <- read.csv(file = "dataset/tk/mouse/GO/Study2_125I-l-GO 914nm.csv")
     Obs.df <- Obs.df[1:5,]
     PDOSE <- Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -152,7 +152,7 @@ read_observation_data <- function(np_name) {
   } 
   else if (np_name == "TiO2: Study1_385nm_10mg/kg") {
     #------------------TiO2: Study1: 385 nm-------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/TiO2/Study1-TiO2.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/TiO2/Study1-TiO2.csv") 
     Obs.df <- Obs.df[1:5,]
     PDOSE = Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -162,7 +162,7 @@ read_observation_data <- function(np_name) {
   } 
   else if (np_name == "TiO2: Study2_220nm_60mg/kg") {
     #------------------TiO2: Study1: 100 nm-------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/TiO2/Study2-TiO2.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/TiO2/Study2-TiO2.csv") 
     Obs.df = Obs.df[1:3,]
     PDOSE = Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -172,7 +172,7 @@ read_observation_data <- function(np_name) {
   } 
   else if (np_name == "FeO: Study1_29nm_5mg/kg") {
     #------------------IO: Study1: 29 nm-------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/iron/Study2_EDT_IONP_29nm.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/iron/Study2_EDT_IONP_29nm.csv") 
     Obs.df <- Obs.df[1:4,]
     PDOSE = Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
@@ -187,7 +187,7 @@ read_observation_data <- function(np_name) {
   } 
   else if (np_name == "FeO: Study2_41nm_4mg/kg") {
     #------------------IO: Study1: 29 nm-------------------------
-    Obs.df <- read.csv(file ="dataset_1/tk/mouse/iron/Study3_USPIO_41nm.csv") 
+    Obs.df <- read.csv(file ="dataset/tk/mouse/iron/Study3_USPIO_41nm.csv") 
     Obs.df <- Obs.df[1:12,]
     PDOSE = Obs.df$Dose.mg.kg.[1]
     pathway <- Obs.df$Input.pathway[1]
